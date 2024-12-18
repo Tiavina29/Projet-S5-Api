827757de2a7343d6676b6638bb79ab7235450a38
ManoarySarobidy <manoarysarobidy@gmail.com>
2024-12-18 21:32:24 +0300

    Initialisation Projet

---
+++ src/main/java/p16/utilisateurs/identifiant/IdentifiantApplication.java
@@ -0,0 +1,13 @@
+package p16.utilisateurs.identifiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentifiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentifiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/p16/utilisateurs/identifiant/IdentifiantApplicationTests.java
@@ -0,0 +1,13 @@
+package p16.utilisateurs.identifiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentifiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
